423f442c484c07ba3987c0b9dc97508a0c759af0
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2021-07-15 02:18:38 +0700

    :sparkles: Support dynamic increment different type :fried_egg:

--- api/record-usage/index.py
+++ api/record-usage/index.py
@@ -5,7 +5,6 @@ from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 hasura_admin_secret = os.getenv("HASURA_ADMIN_SECRET", "")
@@ -13,10 +12,6 @@ hasura_graphql_server = os.getenv("HASURA_GRAPHQL_SERVER", "")
 hasura_graphql_server_ip = os.getenv("HASURA_GRAPHQL_SERVER_IP", "")
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[hasura_graphql_server_ip],
-)
 
 headers = {
     'content-type': 'application/json',
@@ -27,8 +22,8 @@ client = Client(transport=transport, fetch_schema_from_transport=True)
 
 search_date_query = gql(
     """
-    query getUsage ($date: date) {
-      usage(where: {date: {_eq: $date}}) {
+    query getUsage ($date: date, $type: String, $owned_by: String) {
+      usage(where: {date: {_eq: $date}, type: {_eq: $type}, owned_by: {_eq: $owned_by}}) {
         id
         date
         type
@@ -36,7 +31,7 @@ search_date_query = gql(
         owned_by
       }
     }
-"""
+    """
 )
 create_usage_mutation = gql(
     """
@@ -45,15 +40,15 @@ create_usage_mutation = gql(
             affected_rows
         }
     }
-"""
+    """
 )
 increment_usage_mutation = gql(
     """
     mutation incrementUsageMutation ($date: date, $type: String, $owned_by: String) {
-    update_usage(_inc: {count: 1}, where: {date: {_eq: $date}, type: {_eq: $type}, owned_by: {_eq: $owned_by}}){
-        affected_rows
+        update_usage(_inc: {count: 1}, where: {date: {_eq: $date}, type: {_eq: $type}, owned_by: {_eq: $owned_by}}){
+            affected_rows
+        }
     }
-}
     """
 )
 
@@ -68,29 +63,40 @@ class EventTriggerPayload(BaseModel):
 
 @app.post("/api/{full_path:path}")
 def record_usage(event_trigger_payload: EventTriggerPayload):
-    print(event_trigger_payload)
     date = event_trigger_payload.created_at.strftime('%Y-%m-%d')
     owned_by = event_trigger_payload.event['data']['new']['owned_by']
-    response = client.execute(
-        search_date_query, variable_values={"date": date}
+    type = event_trigger_payload.trigger['name']\
+        .replace('record_', '')\
+        .replace('_usage', '')
+    usage = client.execute(
+        search_date_query, variable_values={
+            "date": date, "type": type, "owned_by": owned_by}
     )
+    is_usage_exist = len(usage['usage'])
 
-    if response['usage']:
-        response = client.execute(
-            increment_usage_mutation, variable_values={
-                "date": date, "type": "send_email", "owned_by": owned_by}
-        )
-        if response['update_usage']['affected_rows'] == 1:
-            return HTMLResponse(status_code=204)
-        else:
-            return HTMLResponse(status_code=500)
-
+    if is_usage_exist:
+        return _increment_usage(date, owned_by, type)
     else:
-        response = client.execute(
-            create_usage_mutation, variable_values={
-                "date": date, "type": "send_email", "owned_by": owned_by}
-        )
-        if response['insert_usage']['affected_rows'] == 1:
-            return HTMLResponse(status_code=204)
-        else:
-            return HTMLResponse(status_code=500)
+        return _create_new_usage(date, owned_by, type)
+
+
+def _create_new_usage(date: str, owned_by: str, type: str):
+    response = client.execute(
+        create_usage_mutation, variable_values={
+            "date": date, "type": type, "owned_by": owned_by}
+    )
+    if response['insert_usage']['affected_rows'] == 1:
+        return HTMLResponse(status_code=204)
+
+    return HTMLResponse(status_code=500)
+
+
+def _increment_usage(date: str, owned_by: str, type: str):
+    response = client.execute(
+        increment_usage_mutation, variable_values={
+            "date": date, "type": type, "owned_by": owned_by}
+    )
+    if response['update_usage']['affected_rows'] == 1:
+        return HTMLResponse(status_code=204)
+
+    return HTMLResponse(status_code=500)
