5e40c10bcc285bcf1cac1408fcfd58f986132a11
gomgam <gam@prontomarketing.com>
2021-07-15 21:30:35 +0700

    :sparkles: Create send email event webhook :dancers:

--- api/send-email/index.py
+++ api/send-email/index.py
@@ -1,6 +1,17 @@
+import os
 import datetime
+
+import sendgrid
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from jinja2 import Environment, FileSystemLoader
 from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+SENDGRID_API_KEY = os.getenv('api_key', '')
 
 
 class EventTriggerPayload(BaseModel):
@@ -11,11 +22,44 @@ class EventTriggerPayload(BaseModel):
     event: dict
 
 
-app = FastAPI()
-
-
-@app.post("/api/{full_path:path}")
+@app.post("/api/send-email/")
 def send_email(event_trigger_payload: EventTriggerPayload):
-    print('################ Sending email function Running #################')
-    print(event_trigger_payload)
-    return 'Success!'
+    question = 'How would you rate your latest experience with Simplesat?'
+
+    personalizations = []
+    for receiver in event_trigger_payload.event['data']['new']['batch']:
+        file_loader = FileSystemLoader('templates')
+        env = Environment(loader=file_loader)
+        template = env.get_template('email_template.html')
+        content = template.render(
+            question=question,
+            survey_id=event_trigger_payload.event['data']['new']['survey_id'],
+            ticket_id=event_trigger_payload.event['data']['new']['ticket_id']
+        )
+
+        sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+        data = {
+            'personalizations': [
+                {
+                    'to': [
+                        {
+                            'email': receiver['email']
+                        }
+                    ],
+                    'subject': question
+                }
+            ],
+            'from': {
+                'email': 'noreply@simplesat.io'
+            },
+            'content': [
+                {
+                    'type': 'text/html',
+                    'value': content
+                }
+            ]
+        }
+
+        sg.client.mail.send.post(request_body=data)
+
+    return HTMLResponse(status_code=200)
